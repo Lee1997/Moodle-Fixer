8834eac7ca02ffe2e906c23782ddacb0c2fe9490
Lee Laffan <leeinform@gmail.com>
2016-11-24 12:47:53 +0000

    Some minor design changes and comments the code so it's easier to understand

--- fixer.js
+++ fixer.js
@@ -2,10 +2,10 @@
     //var inStr = "CS265[A] - Software Testing (2017: S1)";
     //var inStr = "MT212A[A] - Linear Algebra 2 (A) (2017:S2)";
 
+    //Selects the courses
     var courses = document.getElementsByClassName("course_title");
     for(var j = 1; j < courses.length; j++){
-        console.log(courses.length);
-
+        //Selects the link title
         var anchor = courses[j].getElementsByTagName('a');
         var inStr = anchor.item(1).title;
 
@@ -14,18 +14,27 @@
 
         for (var i = 0; i < inStr.length - 3; i++) {
             if (inStr.substring(i, i + 3) == "[A]") {
-                fullName += inStr.substring(inStr.length - 3, inStr.length - 1);
+
+                //Adds in the Module Code
+                fullName += inStr.substring(0, 5);
                 fullName += " - ";
+
+                //Adds in the Module Name
                 fullName += inStr.substring(i + 6, inStr.length - 10);
                 fullName += " - ";
-                fullName += inStr.substring(0, 5);
+
+                //Adds in the Semester the module is covered
+                fullName += inStr.substring(inStr.length - 3, inStr.length - 1);
+
+                //Enables you to replace the code
                 fullCourse = true;
+
                 break;
             }
         }
+        //Changes the code if it's a proper module
         if(fullCourse){
             anchor[1].innerHTML = fullName;
         }
-
     }
 })();
